81e86ebd9d046b59b83fd956b0b453c766b524ed
DaniilGus <d.daniil@inbox.ru>
2023-04-13 13:27:42 +0300

    add personal_office

--- reg/server_st.php
+++ reg/server_st.php
@@ -76,7 +76,7 @@ if (isset($_POST['login_user'])) {
         if (mysqli_num_rows($results) == 1) {
             $_SESSION['username'] = $username;
             $_SESSION['success'] = "You are now logged in";
-            header('location: index_st.php');
+            header('location: /PD/website/personal_office_student.html');
         }else {
             array_push($errors, "Wrong username/password combination");
         }
